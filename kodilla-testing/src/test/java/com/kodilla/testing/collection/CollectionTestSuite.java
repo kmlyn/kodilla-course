5c86c9e1ac36d5e07a9a8032795a72bd19e96632
Krzysztof Młynarczyk <11165215+kmlyn@users.noreply.github.com>
2020-10-14 00:03:10 +0200

    6.3. Changed test to use more lists and arrays + added comments given, when, then

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -20,21 +20,30 @@ public class CollectionTestSuite {
     @DisplayName("When given an empty list, another empty list should be returned")
     @Test
     public void testOddNumbersExterminatorEmptyList() {
+        //Given
         List<Integer> l = Arrays.asList();
+
+        //When
         OddNumbersExterminator one = new OddNumbersExterminator();
         List<Integer> result = one.exterminate(l);
+
+        //Then
         Assertions.assertEquals(0, result.size());
     }
 
     @DisplayName("When given a normal list, only even numbers should be returned")
     @Test
     public void testOddNumbersExterminatorNormalList() {
+        //Given
         List<Integer> l = Arrays.asList(1,2,3,4,5,6,7);
+
+        //When
         OddNumbersExterminator one = new OddNumbersExterminator();
         List<Integer> result = one.exterminate(l);
+
+        //Then
+        List<Integer> expected = Arrays.asList(2,4,6);
         Assertions.assertEquals(3, result.size());
-        Assertions.assertEquals(2, result.get(0));
-        Assertions.assertEquals(4, result.get(1));
-        Assertions.assertEquals(6, result.get(2));
+        Assertions.assertArrayEquals(expected.toArray(), result.toArray());
     }
 }

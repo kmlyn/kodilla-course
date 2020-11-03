ccaa1e88f715deadb7c3ed0d1266b72975ff1097
Krzysztof Młynarczyk <11165215+kmlyn@users.noreply.github.com>
2020-11-03 22:48:19 +0100

    7.6. Using Stream: exchanged Period to ChronoUnit

--- kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.stream.portfolio;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -90,7 +91,7 @@ class BoardTestSuite {
       double averageWorkTime = project.getTaskLists().stream()
               .filter(inProgressTasks::contains)
               .flatMap(tl -> tl.getTasks().stream())
-              .mapToLong(t -> Period.between(t.getCreated(), LocalDate.now()).getDays())
+              .mapToLong(t -> ChronoUnit.DAYS.between(t.getCreated(), LocalDate.now()))
               .average().getAsDouble();
 
       //Then

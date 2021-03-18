dffa812f02f5010d31a46efd387c225077e948ab
Krzysztof Młynarczyk <11165215+kmlyn@users.noreply.github.com>
2021-03-18 21:14:38 +0100

    8.4. findFlight() now returns a boolean

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -4,14 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FlightFinder {
-    public void findFlight(Flight flight) throws RouteNotFoundException {
+    public Boolean findFlight(Flight flight) throws RouteNotFoundException {
         Map<String, Boolean> airports = new HashMap<>();
         airports.put("Warsaw", true);
         airports.put("Prague", false);
         airports.put("Geneve", true);
         airports.put("Helsinki", true);
 
-        if(!airports.get(flight.getArrivalAirport()) || !airports.get(flight.getDepartureAirport()))
+        if(!airports.containsKey(flight.getArrivalAirport()))
+            return airports.get(flight.getArrivalAirport());
+        else
             throw new RouteNotFoundException();
     }
 }

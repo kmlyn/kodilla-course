3898819049d546361bc797706664e87dd17e9039
Krzysztof Młynarczyk <11165215+kmlyn@users.noreply.github.com>
2021-03-20 00:31:02 +0100

    Spring: less code with @Autowired

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -1,37 +1,29 @@
 package com.kodilla.spring.library;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class LibraryTestSuite {
 
-    @Test
-    void testLoadFromDb() {
-        //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Library library = context.getBean(Library.class);
+    @Autowired
+    private Library library;
 
+    @Test
+    public void testLoadFromDb() {
+        //Given
         //When
         library.loadFromDb();
-
         //Then
         //do nothing
     }
 
     @Test
-    void testSaveToDb() {
+    public void testSaveToDb() {
         //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Library library = context.getBean(Library.class);
-
         //When
         library.saveToDb();
-
         //Then
         //do nothing
     }

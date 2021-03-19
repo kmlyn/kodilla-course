953f90a95930eb4de5d8591f98e31856bf1c7773
Krzysztof Młynarczyk <11165215+kmlyn@users.noreply.github.com>
2021-03-20 00:27:03 +0100

    Spring: DI through constructor

--- kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
@@ -1,9 +1,6 @@
 package com.kodilla;
 
-import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
-import com.kodilla.spring.shape.Square;
-import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -19,7 +16,7 @@ class KodillaSpringApplicationTests {
 		//Given
 		ApplicationContext context =
 				new AnnotationConfigApplicationContext("com.kodilla.spring");
-		Shape shape = context.getBean(Circle.class);
+		Shape shape = (Shape)context.getBean("circle");
 
 		//When
 		String name = shape.getShapeName();
@@ -33,7 +30,7 @@ class KodillaSpringApplicationTests {
 		//Given
 		ApplicationContext context =
 				new AnnotationConfigApplicationContext("com.kodilla.spring");
-		Shape shape = context.getBean(Triangle.class);
+		Shape shape = (Shape)context.getBean("triangle");
 
 		//When
 		String name = shape.getShapeName();
@@ -47,15 +44,27 @@ class KodillaSpringApplicationTests {
 		//Given
 		ApplicationContext context =
 				new AnnotationConfigApplicationContext("com.kodilla.spring");
-		Shape shape = context.getBean(Square.class);
+		Shape shape = (Shape)context.getBean("createSquare");
+
 		//When
 		String name = shape.getShapeName();
+
 		//Then
 		assertEquals("This is a square.", name);
 	}
 
 	@Test
-	void contextLoads() {
+	void testShapeLoadedIntoContainer() {
+		//Given
+		ApplicationContext context =
+				new AnnotationConfigApplicationContext("com.kodilla.spring");
+		Shape shape = (Shape)context.getBean("chosenShape");
+
+		//When
+		String name = shape.getShapeName();
+
+		//Then
+		System.out.println("Chosen shape says: " + name);
 	}
 
 }

e951298c4403700e28dd66b0eb23ef84842138a0
Krzysztof Młynarczyk <11165215+kmlyn@users.noreply.github.com>
2021-03-20 02:12:11 +0100

    14.2. Fixing assertEquals argument order

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -15,9 +15,9 @@ public class CalculatorTestSuite {
     public void testCalculations() {
         //Given
         //When & Then
-        assertEquals(calc.add(1.0, 4.5), 5.5);
-        assertEquals(calc.sub(4.5, 5.5), -1.0);
-        assertEquals(calc.mul(2.0, 3.5), 7.0);
-        assertEquals(calc.div(5.0, 2.5), 2.0);
+        assertEquals(5.5, calc.add(1.0, 4.5));
+        assertEquals(-1.0, calc.sub(4.5, 5.5));
+        assertEquals(7.0, calc.mul(3.5, 2.0));
+        assertEquals(2.0, calc.div(5.0, 2.5));
     }
 }

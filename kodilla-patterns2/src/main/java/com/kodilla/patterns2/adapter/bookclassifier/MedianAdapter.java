a885fbe05bf85b0950ca63ed91833f6c36557e00
Krzysztof Młynarczyk <11165215+kmlyn@users.noreply.github.com>
2021-03-29 08:36:15 +0200

    24.5. Adapter pattern: bookclassifier; fixed imports

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
@@ -1,6 +1,5 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
-import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
 import com.kodilla.patterns2.adapter.bookclassifier.librarya.Classifier;
 import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature;
 
@@ -10,11 +9,12 @@ import java.util.Set;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier {
     @Override
-    public int publicationYearMedian(Set<Book> bookSet) {
+    public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclassifier.librarya.Book> bookSet) {
         Map<BookSignature, com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book> bookMap = new HashMap<>();
-        for (Book b:
+        for (com.kodilla.patterns2.adapter.bookclassifier.librarya.Book b:
              bookSet) {
-            bookMap.put(new BookSignature(b.getSignature()), new com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book(
+            bookMap.put(new BookSignature(b.getSignature()),
+                    new com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book(
                     b.getAuthor(), b.getTitle(), b.getPublicationYear()
             ));
         }
